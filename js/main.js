3428fa150a6229394df351148d6ce40e980f05d9
RezNick98 <ezequiel.buzzella@gmail.com>
2022-09-19 11:05:17 -0300

    Terminado captcha y formulario

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 "uses strict"
+let number1=document.querySelector("#number1").innerHTML=Math.floor(Math.random()*6);
+let number2=document.querySelector("#number2").innerHTML=Math.floor(Math.random()*6);
+let resultado=number1+number2;
+document.querySelector("#send").addEventListener("click",enviarForm);
 document.querySelector('#mensaje').addEventListener('click',()=>{
     let elem = document.querySelector('.form')
     elem.classList.toggle("mostrarFormulario")
-    console.log("Hola")
 })
+function enviarForm(){
+    event.preventDefault();
+    let input_result=Number(document.querySelector("#result").value);
+    if(input_result==resultado){
+        document.querySelector("#send_status").innerHTML="Su consulta fue enviada";
+    }else{
+        document.querySelector("#send_status").innerHTML="Captcha erroneo";
+    }
+}
